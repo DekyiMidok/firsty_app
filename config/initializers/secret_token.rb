c8fdc3151d1faad97bcaf096fa41e5fd3a8a8682
Zac Smith <zac@dharmaocean.org>
2013-05-11 23:40:34 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstyApp::Application.config.secret_token = '00834c7f97ce332c21bc2039f6ba1231a319d55a73fec95f5f04f6401ff80a9302499b550f0a4dccf8efe2ba2364bfb36fb555444764e517f8b08d7c2b2eb219'
